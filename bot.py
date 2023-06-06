17fb6983349f067f547d145d511ed522b9f8a7cf
IbrahimAzab <54166348+simplehima@users.noreply.github.com>
2023-06-06 17:10:56 +0300

    updated code uses a separate connect_to_voice_channel function to handle the connection to the voice channel. It continuously attempts to connect and retries every 10 seconds if a connection error occurs. This way, the bot will keep trying to establish a good connection until successful.

--- bot.py
+++ bot.py
@@ -1,49 +1,76 @@
+import os
 import discord
+from dotenv import load_dotenv
 from discord.ext import commands
 import asyncio
-import os
 from collections import deque
 
-intents = discord.Intents.all()
-intents.members = True
+load_dotenv()
 
-TOKEN = "YOUR BOT TOKEN"
-MUSIC_DIR = "PATH/music" # Replace with your music directory path
-VC_NAME = "VOICE CHANNEL NAME"
-TXT_CHANNEL_ID = 123456789 # Replace with your text channel ID
+TOKEN = os.getenv('TOKEN')
+MUSIC_DIR = os.getenv('MUSIC_DIR')
+VC_NAME = os.getenv('VC_NAME')
+TXT_CHANNEL_ID = int(os.getenv('TXT_CHANNEL_ID'))
 
-bot = commands.Bot(command_prefix='!', intents=intents)
+bot = commands.Bot(command_prefix='!', intents=discord.Intents.all())
 
 music_queue = deque()
 
+async def connect_to_voice_channel(channel):
+    while True:
+        try:
+            voice = await channel.connect()
+            print(f"Connected to voice channel: {channel.name}")
+            return voice
+        except Exception as e:
+            print(f"Failed to connect to voice channel: {channel.name} ({e})")
+            await asyncio.sleep(10)  # Retry after 10 seconds
+
 @bot.event
 async def on_ready():
-    print(f"Logged in as {bot.user.name}")
+    print(f'{bot.user.name} has connected to Discord!')
 
 @bot.event
 async def on_voice_state_update(member, before, after):
     if member == bot.user:
-        return # Ignore updates caused by the bot itself
-    global music_queue
+        return  # Ignore updates caused by the bot itself
     if before.channel != after.channel:
         if after.channel is not None and after.channel.name == VC_NAME:
-            voice = await after.channel.connect()
+            voice = discord.utils.get(bot.voice_clients, guild=after.channel.guild)
+            if voice and voice.is_connected():
+                await voice.move_to(after.channel)
+            else:
+                voice = await connect_to_voice_channel(after.channel)
+            
+            await bot.get_channel(TXT_CHANNEL_ID).send(f"{member.mention} is feeling lonely and joined {VC_NAME}. ")
+            
             while len(music_queue) > 0 or any(m.voice != None and m.voice.channel == voice.channel for m in voice.guild.members):
                 if len(music_queue) == 0:
                     for file in os.listdir(MUSIC_DIR):
                         if file.endswith(".mp3"):
                             music_queue.append(os.path.join(MUSIC_DIR, file))
+                
                 file = music_queue.popleft()
                 voice.play(discord.FFmpegPCMAudio(file))
                 while voice.is_playing():
                     await asyncio.sleep(1)
-            await voice.disconnect()
-            await bot.get_channel(TXT_CHANNEL_ID).send(f"{member.mention} is feeling lonely and joined {VC_NAME}.")
-        elif before.channel is not None and before.channel.name == VC_NAME and len(before.channel.members) == 1:
-            voice = discord.utils.get(bot.voice_clients, guild=before.channel.guild)
-            if voice.is_connected():
+            
+            if len(voice.channel.members) == 1:
                 await voice.disconnect()
-            await bot.get_channel(TXT_CHANNEL_ID).send(f"{member.mention} i hope that you left happy or better 💕❤.")
-            music_queue = deque()
+                music_queue.clear()
+            else:
+                await bot.get_channel(TXT_CHANNEL_ID).send(f"I am leaving 🥺・Lonely People.")
+        
+        elif before.channel is not None and before.channel.name == VC_NAME:
+            if len(before.channel.members) == 1:
+                voice = discord.utils.get(bot.voice_clients, guild=before.channel.guild)
+                if voice and voice.is_connected():
+                    await voice.disconnect()
+                    music_queue.clear()
+                
+                await bot.get_channel(TXT_CHANNEL_ID).send(f"{member.mention} I hope that you left happy or better 💕❤.")
+            
+            else:
+                await bot.get_channel(TXT_CHANNEL_ID).send(f"{member.mention} left {VC_NAME}.")
 
 bot.run(TOKEN)
